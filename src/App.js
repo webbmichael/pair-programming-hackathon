5ad0aa5e04b9b462cfaed96c72bb84b2adffc410
Michael Webb <webbmichael@hotmail.co.uk>
2022-03-10 15:29:30 +0000

    setupe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Header from "./componenets/Header/Header";
+import { Redirect, Route, Switch } from "react-router-dom";
+
+
+import Swipe from "./componenets/Swipe/Swipe";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+      <Header    />
+      <Route 
+          path={`/`}
+          exact
+          render ={(routerProps)=>{
+            return <Login  {...routerProps}/>
+          }} />
+          <Route 
+          path={`/swipe`}
+          render ={(routerProps)=>{
+            return <Swipe    {...routerProps}/>
+       }}/>
+        
+      
+
+
+    </>
+    
   );
 }
 
