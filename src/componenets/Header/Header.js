6e605f00b30191ffdce89e2e69fcb4168bfcb769
Michael Webb <webbmichael@hotmail.co.uk>
2022-03-10 20:53:29 +0000

    this

--- src/componenets/Header/Header.js
+++ src/componenets/Header/Header.js
@@ -4,16 +4,22 @@ import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import ForumIcon from '@mui/icons-material/Forum';
 import NightlifeTwoToneIcon from '@mui/icons-material/NightlifeTwoTone';
 import './Header.scss'
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+// import IconButton from "@material-ui/core/IconButton";
+
 
 
 export default class Header extends Component {
   render() {
     return (
         <div className='header'>
-            <PersonOutlineIcon className='header__icon'/>
+         
+                <PersonOutlineIcon className='header__icon'/>
+           
             <NightlifeTwoToneIcon/>
-            <ForumIcon className='header__forum'/>
-            
+            <Link to="/messages">
+                <ForumIcon className='header__forum'/>
+            </Link>
 
         </div>
 
